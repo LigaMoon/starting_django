28595854680b22d5b2377e668638e49f59a03f5b
Liga <ligabaikova@gmail.com>
2020-11-24 21:47:24 +0000

    prepare to be deployed to heroku

---
+++ todo/test_forms.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from .forms import ItemsForm
+
+
+class TestItemsForms(TestCase):
+
+    def test_item_name_is_required(self):
+        form = ItemsForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        form = ItemsForm({'name': 'Test todo Item'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ItemsForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
